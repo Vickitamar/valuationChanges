7155ed748e6137ddc9bc34d7ee8c98c91b1940e3
Vicki Edwards <vicki.edwards@quadrotech-it.com>
2020-12-06 17:42:27 +0000

    formatting dates and percentages

--- components/valuationChanges.js
+++ components/valuationChanges.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { lightFormat } from "date-fns";
 import {
     AccountLabel,
     AccountList,
@@ -9,19 +10,30 @@ import {
   } from "./style";
 
 const ValuationChanges = ({account}) => {
+    
+    const sincePurchase = account.recentValuation.amount - account.originalPurchasePrice
+    const sincePurchaseFormatted = new Intl.NumberFormat().format(sincePurchase);
+    const sincePurchasePercentage = sincePurchase / account.originalPurchasePrice;
+    const originalPrice = new Intl.NumberFormat().format(account.originalPurchasePrice)
+    const todaysDate = parseInt(lightFormat(new Date(), 'yyyy-MM-dd'));
+    const originalDate = parseInt(account.originalPurchasePriceDate);
+    const numberOfYears = todaysDate - originalDate;
+    const annualAppreciation = sincePurchasePercentage / numberOfYears;
+
+
     return (
         <>
           <AccountLabel>Valuation Changes</AccountLabel>
           <RowContainer>
             <AccountList>
-              <InfoText>Purchased for £199,500 in September 2015</InfoText>
+              <InfoText>Purchased for £{originalPrice}  in September 2015</InfoText>
               <StyledInfoWrapper>
               <InfoText>Since purchase</InfoText>
-              <InfoFigure>£50,00</InfoFigure>
+                <InfoFigure>£{sincePurchaseFormatted} ({sincePurchasePercentage}%)</InfoFigure>
               </StyledInfoWrapper>
               <StyledInfoWrapper>
               <InfoText>Annual appreciation</InfoText>
-              <InfoFigure>£50,00</InfoFigure>
+                <InfoFigure>{annualAppreciation}%</InfoFigure>
               </StyledInfoWrapper>
             </AccountList>
           </RowContainer>
