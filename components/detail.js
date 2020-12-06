114dcff359e8fea51c06946aa99bff8aa3a51221
Vicki Edwards <vicki.edwards@quadrotech-it.com>
2020-12-06 15:47:04 +0000

    using getServerSideProps

--- components/detail.js
+++ components/detail.js
@@ -7,38 +7,13 @@ import Property from "./property";
 import Mortgage from "./mortgage";
 import { AccountSection, Inset } from "./style";
 
-const account = {
-  uid: "65156cdc-5cfd-4b34-b626-49c83569f35e",
-  deleted: false,
-  dateCreated: "2020-12-03T08:55:33.421Z",
-  currency: "GBP",
-  name: "15 Temple Way",
-  bankName: "Residential",
-  type: "properties",
-  subType: "residential",
-  originalPurchasePrice: 250000,
-  originalPurchasePriceDate: "2017-12-03",
-  recentValuation: { amount: 310000, status: "good" },
-  associatedMortgages: [
-    {
-      name: "HSBC Repayment Mortgage",
-      uid: "fb463121-b51a-490d-9f19-d2ea76f05e25",
-      currentBalance: -175000,
-    },
-  ],
-  canBeManaged: false,
-  postcode: "BS1 2AA",
-  lastUpdate: "2020-12-01T08:55:33.421Z",
-  updateAfterDays: 30,
-};
-
-const Detail = ({}) => {
-  useEffect(() => {
-    window
-      .fetch("/api/account")
-      .then((res) => res.json())
-      .then(console.log);
-  }, []);
+const Detail = ({ account }) => {
+  // useEffect(() => {
+  //   window
+  //     .fetch("/api/account")
+  //     .then((res) => res.json())
+  //     .then(console.log);
+  // }, []);
 
   let mortgage;
   const lastUpdate = new Date(account.lastUpdate);
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,56 @@
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import Detail from "../components/detail";
 import { Banner } from "../components/banner";
+import Property from "../components/property";
 
 const Main = styled.main`
   max-width: 960px;
   margin: 0 auto;
 `;
 
-export default function PropertyDetails() {
+const PropertyDetails = ({ account }) => {
   return (
     <Main>
       <Banner>Property details</Banner>
-      <Detail />
+      <Detail account={account}/>
     </Main>
   );
 }
+
+export async function getServerSideProps(context) {
+  //  const res = await fetch(`"/api/account"`)
+  //  const data = await res.json()
+  const account = {
+    uid: "65156cdc-5cfd-4b34-b626-49c83569f35e",
+    deleted: false,
+    dateCreated: "2020-12-03T08:55:33.421Z",
+    currency: "GBP",
+    name: "15 Temple Way",
+    bankName: "Residential",
+    type: "properties",
+    subType: "residential",
+    originalPurchasePrice: 250000,
+    originalPurchasePriceDate: "2017-12-03",
+    recentValuation: { amount: 310000, status: "good" },
+    associatedMortgages: [
+      {
+        name: "HSBC Repayment Mortgage",
+        uid: "fb463121-b51a-490d-9f19-d2ea76f05e25",
+        currentBalance: -175000,
+      },
+    ],
+    canBeManaged: false,
+    postcode: "BS1 2AA",
+    lastUpdate: "2020-12-01T08:55:33.421Z",
+    updateAfterDays: 30,
+  };
+   return {
+     props: { account }
+   }
+  
+  }
+
+
+
+export default PropertyDetails;
