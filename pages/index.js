1e94d203e35150a073e46f231e3353a5dd902587
Vicki Edwards <vicki.edwards@quadrotech-it.com>
2020-12-07 09:28:28 +0000

    removed useeffect

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import Detail from "../components/detail";
 import { Banner } from "../components/banner";
